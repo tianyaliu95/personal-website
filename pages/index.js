3cbd4a11ced582ed72273c527ead96166ad8f3b4
tianyaliu95 <tianyaliu1995@gmail.com>
2022-02-03 01:03:44 -0500

    styling fix

--- pages/index.js
+++ pages/index.js
@@ -25,19 +25,19 @@ export async function getServerSideProps ({ req, res, query }) {
                 <html lang="en">
                 
                 <head>
-                    <meta charset="utf-8">
-                    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-                    <meta name="viewport" content="width=device-width, initial-scale=1">
-                    <title>Tianya Liu</title>
-                    <link rel="stylesheet" href="http://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.1.0/css/font-awesome.min.css">
-                    <link rel="stylesheet" href="http://maxcdn.bootstrapcdn.com/bootstrap/3.2.0/css/bootstrap.min.css">
-                    <link rel="stylesheet" href="./style.css">
-                    <link rel="shortcut icon" href="./img/logo.jpg">
-                    <link href='http://fonts.googleapis.com/css?family=Lato' rel='stylesheet' type='text/css'>
-                    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.7.1/jquery.js"></script>
-                    <script src="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/3.2.0/js/tooltip.js"></script>
-                    <script type="text/javascript" src="./js/jquery.snow.js"></script>
-                </head>
+                  <meta charset="utf-8">
+                  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+                  <meta name="viewport" content="width=device-width, initial-scale=1">
+                  <title>Tianya Liu</title>
+                  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.1.0/css/font-awesome.min.css">
+                  <link rel="stylesheet" href="./style.css">
+                  <link rel="shortcut icon" href="./img/logo.jpg">
+                  <link href='https://fonts.googleapis.com/css?family=Lato' rel='stylesheet' type='text/css'>
+                  <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.8/jquery.js"></script>
+                  <script src="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/3.2.0/js/tooltip.js"></script>
+                  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.2.0/css/bootstrap.min.css">
+                  <script type="text/javascript" src="./js/jquery.snow.js"></script>
+              </head>
                 
                 <body id="snow">
                 <!--     <script>
