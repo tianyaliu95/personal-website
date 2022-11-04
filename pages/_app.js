167fb8c358cf691a477d458918211dbe42b7e924
tianyaliu95 <tianyaliu1995@gmail.com>
2022-11-04 02:55:55 -0400

    UA tracking fix

--- pages/_app.js
+++ pages/_app.js
@@ -3,6 +3,17 @@ import Script from 'next/script'
 function MyApp({ Component, pageProps }) {
   return (
     <>
+      <Script async src="https://www.googletagmanager.com/gtag/js?id=UA-155298612-1" />
+      <Script
+        onLoad={() => {
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+
+          gtag('config', 'UA-155298612-1');
+        }} 
+      />
+
       <Script async src="https://www.googletagmanager.com/gtag/js?id=G-70DBW1V0HN" />
       <Script
         onLoad={() => {
@@ -13,6 +24,7 @@ function MyApp({ Component, pageProps }) {
           gtag('config', 'G-70DBW1V0HN');
         }} 
       />
+
       <Component {...pageProps} />
     </>
     
