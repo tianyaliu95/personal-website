13a357af550199e4f45a6b74b98d8900a07a1d09
tianyaliu95 <tianyaliu1995@gmail.com>
2024-02-27 20:58:07 -0800

    update

--- pages/index.js
+++ pages/index.js
@@ -3,76 +3,81 @@ import { useState, useEffect } from 'react'
 
 import { Lightbox } from 'react-modal-image'
 
-export default function Home({}) {
-    const [htmlFile, setHtmlFile] = useState()
-	const [isModalOpen, setModalOpen] = useState(false)
+export default function Home({ }) {
+  const [htmlFile, setHtmlFile] = useState()
+  const [isContentReady, setContentReady] = useState(false)
+  const [isModalOpen, setModalOpen] = useState(false)
 
-    useEffect(() => {
-        const fetchHtml = async () => {
-            const html = await fetch('/index.html')
-            const htmlString = await html.text()
-            setHtmlFile(htmlString)
-        }
+  useEffect(() => {
+    const fetchHtml = async () => {
+      const html = await fetch('/index.html')
+      const htmlString = await html.text()
+      setHtmlFile(htmlString)
+    }
 
-        fetchHtml()
+    fetchHtml()
+    setTimeout(() => {
+      setContentReady(true)
+    }, 0)
+  })
+
+  const openModal = () => {
+    setModalOpen(true)
+    window.dataLayer && window.dataLayer.push({
+      'event': 'IMAGE_VIEW',
+      'time_stamp': `${new Date()}`
     })
+  }
 
-	const openModal = () => {
-		setModalOpen(true)
-		window.dataLayer && window.dataLayer.push({
-			'event': 'IMAGE_VIEW',
-            'time_stamp': `${new Date()}`
-		})
-	}
+  const closeModal = () => {
+    setModalOpen(false)
+  }
 
-	const closeModal = () => {
-		setModalOpen(false)
-	}
+  const onResumeClick = () => {
+    window.dataLayer && window.dataLayer.push({
+      'event': 'RESUME_VIEW',
+      'time_stamp': `${new Date()}`
+    })
+  }
 
-    const onResumeClick = () => {
-	    window.dataLayer && window.dataLayer.push({
-			'event': 'RESUME_VIEW',
-            'time_stamp': `${new Date()}`
-		})
-	}
+  const onXingzheClick = () => {
+    window.dataLayer && window.dataLayer.push({
+      'event': 'XINGZHEFANGCHE_VIEW',
+      'time_stamp': `${new Date()}`
+    })
+  }
 
-    const onXingzheClick = () => {
-	    window.dataLayer && window.dataLayer.push({
-			'event': 'XINGZHEFANGCHE_VIEW',
-            'time_stamp': `${new Date()}`
-		})
-	}
-
-	useEffect(() => {
-		document.getElementById('avatar')?.addEventListener('click', openModal)
-        document.getElementById('resume')?.addEventListener('click', onResumeClick)
-        document.getElementById('xingzhefangche')?.addEventListener('click', onXingzheClick)
-	})
+  useEffect(() => {
+    document.getElementById('avatar')?.addEventListener('click', openModal)
+    document.getElementById('resume')?.addEventListener('click', onResumeClick)
+    document.getElementById('xingzhefangche')?.addEventListener('click', onXingzheClick)
+  })
 
   return (
     <div>
       <Head>
         <title>Tianya Liu</title>
-        <meta name="description" content="Tianya's personal website" />
+        <meta name="description" content="Welcome to my personal website" />
         <link rel="icon" href="/logo.jpg" />
       </Head>
 
-			{isModalOpen && 
-				<Lightbox
-					large={'/img/avatar.jpg'}
-					onClose={closeModal}
-				/>}
+      {isModalOpen &&
+        <Lightbox
+          large={'/img/head2.jpg'}
+          onClose={closeModal}
+        />}
 
-      <div
-        dangerouslySetInnerHTML={{
-          __html: htmlFile
-        }}
-      />
+      {isContentReady &&
+        <div
+          dangerouslySetInnerHTML={{
+            __html: htmlFile
+          }}
+        />}
     </div>
   )
 }
 
-export async function getServerSideProps ({ req, res, query }) {
+export async function getServerSideProps({ req, res, query }) {
 
   return {
     props: {}
